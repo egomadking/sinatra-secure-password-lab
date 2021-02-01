005f7e91af8670fbe39963ddc79877e86dbe78dd
Roy Mosby <ego.madking@gmail.com>
2021-02-01 14:54:27 -0500

    commit tree 679ad01711bbf1b46314c0b050854a18107b5f83

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -18,6 +18,15 @@ class ApplicationController < Sinatra::Base
 
   post "/signup" do
     #your code here
+    if params[:username].empty?
+      redirect to "/failure"
+    end
+    user = User.new(:username => params[:username], :password => params[:password])
+    if user.save
+      redirect to "/login"
+    else
+      redirect to "/failure"
+    end
 
   end
 
@@ -33,6 +42,16 @@ class ApplicationController < Sinatra::Base
 
   post "/login" do
     ##your code here
+    if params[:username].empty? || params[:password].empty?
+      redirect to "/failure"
+    end
+    user = User.find_by(:username => params[:username])
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect to "/account"
+    else
+      redirect to "/failure"
+    end
   end
 
   get "/failure" do
